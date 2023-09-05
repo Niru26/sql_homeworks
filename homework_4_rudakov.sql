88484163c687dfc7f6de119e8d945e00932a8164
Nikolay <nikolayrudakov@mail.ru>
2023-09-05 12:03:47 +0600

    finished task 1, count total likes quantity

--- homework_4_rudakov.sql
+++ homework_4_rudakov.sql
@@ -1,3 +1,10 @@
 use seminar_geekbrains;
 select *
 from likes;
+-- count total likes, age less than 12
+select 
+	count(media_id) as total_likes
+from likes
+left join profiles
+	using (user_id)
+where birthday > '2011-01-01'
